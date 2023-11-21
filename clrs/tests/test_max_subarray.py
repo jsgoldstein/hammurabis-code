f32053c17a076a100ecfc0e557c71249d3c7e6cb
jsgoldstein <jakegoldstein95@gmail.com>
2023-11-21 13:19:08 -0500

    finish up max subarray stuff

--- clrs/tests/test_max_subarray.py
+++ clrs/tests/test_max_subarray.py
@@ -17,3 +17,15 @@ class TestMaxSubarray:
     @given(lists(integers(), min_size=2, unique=True))
     def test_max_subarray_recursive(self, a: List[int]) -> None:
         assert max_subarray.max_subarray_recursive(a, 0, len(a) - 1) == max_subarray.max_subarray(a)
+
+    @given(lists(integers(), min_size=2, unique=True))
+    def test_max_subarray_index(self, a: List[int]) -> None:
+        assert max_subarray.max_subarray_index(a, 0, len(a)) == max_subarray.max_subarray(a)
+
+    @given(lists(integers(), min_size=2, unique=True))
+    def test_max_subbaray_mixed(self, a: List[int]) -> None:
+        assert max_subarray.max_subbaray_mixed(a, 0, len(a) - 1) == max_subarray.max_subarray(a)
+
+    @given(lists(integers(), min_size=2, unique=True))
+    def test_max_subarray_linear(self, a: List[int]) -> None:
+        assert max_subarray.max_subarray_linear(a) == max_subarray.max_subarray(a)
